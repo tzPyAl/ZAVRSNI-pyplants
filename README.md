df8c02d81f77da2b43b0eaf29c7c5cde2390eb9a
tzPyAl <ude.acc.44@gmail.com>
2023-03-19 08:17:46 +0000

    Endpoint data from ip

--- README.md
+++ README.md
@@ -6,7 +6,16 @@
 - Using [Beautiful Soup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/) we fetch all the meta + image url, and save inside the json file locally
 - access [tropicopia - plant](http://www.tropicopia.com/house-plant/detail.np/detail-01.html) to review the available metadata
 - if you want fresh data, delete the file inside scrapped_data dir, or del the dir
+- TODO: save scrapped data to db
+
+## Endpoint data from ip
+
+- get local data from ip: city, country, longitude, latitude
+- user can hardcode location by city, or lon/lat so weather can be fetched correctly for his plants location
+- TODO: save hardcoded values to db
 
 ## Next step/s
 
-- fetch some location based forecast
+- write tests
+- create ci on gh actions
+- get weather for endpoint location
---
+++ scripts/endpoint_data.py
@@ -0,0 +1,43 @@
+import requests
+
+class Endpoint:
+    def __init__(self, city=None, lon=None, lat=None):
+        if self._get_location_from_ip() == None:
+            self.ip = "0.0.0.0"
+            self.city = "Unknown"
+            self.country = "Unknown"
+            self.lon = "Unknown"
+            self.lat = "Unknown"
+        # hardcode lon/lat and/or city if provided by user
+        if lon != None and lat != None:
+            self.lon = lon
+            self.lat = lat
+        if city != None:
+            self.city = city
+
+    def _get_ip(self):
+        try:
+            _ip_api = requests.get('https://api64.ipify.org?format=json')
+        except:
+            print("No internet connection")
+            return False
+        else:
+            if _ip_api.status_code == 200:
+                self.ip = _ip_api.json()["ip"]
+                return self.ip
+            else:
+                print("Error while fetching external IP address, st:", _ip_api.status_code)
+                return False
+
+    def _get_location_from_ip(self):
+        if self._get_ip():
+            _location_api = requests.get(f'https://ipapi.co/{self.ip}/json/')
+            if _location_api.status_code == 200:
+                self.city = _location_api.json()["city"]
+                self.region = _location_api.json()["region"]
+                self.country = _location_api.json()["country_name"]
+                self.population = _location_api.json()["country_population"]
+                self.lat = _location_api.json()["latitude"]
+                self.lon = _location_api.json()["longitude"]
+                return True
+            
