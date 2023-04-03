03f6c05bd1d4232765691170d6286fed7b9781f3
tzPyAl <ude.acc.44@gmail.com>
2023-04-03 23:03:55 +0100

    flask secret env var

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from dotenv import load_dotenv
+import os
 from libs.wt_forms import RegistrationForm, LoginForm
 
+load_dotenv()
+FLASK_SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = FLASK_SECRET_KEY
 
 pots = [
     {
