04d1c6b8297d818ecc29e27d960b5d04820cc5e1
tzPyAl <ude.acc.44@gmail.com>
2023-04-04 00:00:10 +0100

    db structure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+from datetime import datetime
 import os
 from libs.wt_forms import RegistrationForm, LoginForm
 
@@ -8,6 +10,31 @@ FLASK_SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = FLASK_SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db" # relative path
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    pots = db.relationship("Pots", backref="owner", lazy=True) # lazy - load dana in one go
+
+    def __repr__(self): # how our Object is printed, when we printed it out
+        return f"User('{self.id}', '{self.username}', '{self.email}')"
+    
+class Pots(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    pot_image = db.Column(db.String(60), default="https://www.ikea.com/es/en/images/products/fejka-artificial-potted-plant-in-outdoor-monstera__0614197_pe686822_s5.jpg")
+    data_created = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow)
+    status = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __repr__(self): # how our Object is printed, when we printed it out
+        return f"User('{self.name}', '{self.status}', '{self.data_created}')"
+    
+# clear db -> db.drop_all() db.create_all()
 
 pots = [
     {
